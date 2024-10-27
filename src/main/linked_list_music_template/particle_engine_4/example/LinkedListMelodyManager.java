2b1f609d5f7dc811b9356de4954721646f481cdc
SoKnox <sophieknox1202@gmai.com>
2024-10-27 18:14:52 -0500

    Third Commit

--- src/main/linked_list_music_template/particle_engine_4/example/LinkedListMelodyManager.java
+++ src/main/linked_list_music_template/particle_engine_4/example/LinkedListMelodyManager.java
@@ -1,3 +1,32 @@
-public class LinkedListMelodyManager {
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+
+
+
+public class LinkedListMelodyManager extends MelodyManager {
+
+    static FileSystem sys = FileSystems.getDefault();
+    static String prependPath = "mid" + sys.getSeparator();
+    static String appendType = ".mid" + sys.getSeparator();
+    
+    String[] files = {"motive1Am", "motive2Am", "motive3Am","motive1E","motive2E", "motive3E"};
+
+    LinkedListMelodyManager()
+    {
+        super();
+    }
+
+    void setup()
+    {
+        for(int i=0; i<files.length; i++)
+        {
+            addMidiFile(prependPath+files[i]+appendType);
+        }
+    }
+
+    int size()
+    {
+        return files.length;
+    }
 
 }
